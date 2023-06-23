a781597832f682a68eb0ca19fa1464b9a85f9437
Tanmay Patwardhan <tanmay.xvii@gmail.com>
2023-06-22 19:08:06 -0700

    Started UI and finished expression eval algorithm

--- calculator/script.js
+++ calculator/script.js
@@ -5,20 +5,24 @@ function precedence(a , b) {
     else return false;
 }
 
+function isOperator(char) {
+    return char === '+' || char === '-' || char === '/' || char === '*';
+}
 
-function toPostfix(str) {
-    let result = "";
+function toPostfix(str1) {
+    let str = str1.split(" ");
+    let result = [];
     let arr = [];
 
     for(let i = 0; i < str.length; i++) {
-        if(str[i] != '+' && str[i] != '-' && str[i] != '/' && str[i] != '*' && str[i] != ' ') result+= str[i];
-        else if(str[i] != ' ') {
+        if(!isOperator(str[i])) result.push(str[i]);
+        else {
             if(arr.length === 0 || precedence(str[i] , arr[arr.length - 1])) {
                 arr.push(str[i]);
             } else {
                 while(arr.length > 0 && !precedence(str[i] , arr[arr.length - 1])) {
                     let curr = arr.pop();
-                    result+=curr;
+                    result.push(curr);
                 }
                 arr.push(str[i]);
             }
@@ -27,16 +31,32 @@ function toPostfix(str) {
 
     while(arr.length > 0) {
         let curr = arr.pop();
-        result+=curr;
+        result.push(curr);
     }
     return result;
 }
 
 
+
 function evaluate(str) {
     let result = 0;
     let arr = [];
+
     for(let i = 0; i < str.length; i++) {
-        if(str[i] >= '0' && str[i] <= '9')
+        if(!isOperator(str[i])) arr.push(parseFloat(str[i]));
+        else {
+            let num1 = arr.pop();
+            let num2 = arr.pop();
+
+            if(str[i] === '+') arr.push(num1 + num2);
+            else if(str[i] === '-') arr.push(num2 - num1);
+            else if(str[i] === '*') arr.push(num1 * num2);
+            else arr.push(num2 / num1);
+        }
     }
+    return arr[0];
+   
 }
+
+
+
